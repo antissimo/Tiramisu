31ce251fdee4c3e8b92ff64033a0dab99a865c67
Antissimo <Boskovicante1@gmail.com>
2025-12-06 19:34:24 +0100

    Dodan logo firme

---
+++ app/config/footer.js
@@ -0,0 +1,45 @@
+
+export const footerConfig = {
+  socials: [
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/tiramisubarsplit/',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M7 2C4.243 2 2 4.243 2 7v10c0 2.757 2.243 5 5 
+          5h10c2.757 0 5-2.243 5-5V7c0-2.757-2.243-5-5-5H7zm10 
+          2c1.654 0 3 1.346 3 3v10c0 1.654-1.346 3-3 
+          3H7c-1.654 0-3-1.346-3-3V7c0-1.654 1.346-3 
+          3-3h10zm-5 3a5 5 0 100 10 5 5 0 
+          000-10zm0 2a3 3 0 110 6 3 3 0 
+          010-6zm4.5-.25a1.25 1.25 0 11-2.5 0 
+          1.25 1.25 0 012.5 0z"/>
+        </svg>
+      ),
+    },
+    {
+      name: 'Facebook',
+      url: 'https://www.facebook.com/p/Tiramis%C3%B9-Bar-Split-61575614048707/',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M22 12a10 10 0 10-11.5 9.9v-7h-2v-3h2v-2.3c0-2 
+          1.2-3.1 3-3.1.9 0 1.8.1 1.8.1v2h-1c-1 
+          0-1.3.6-1.3 1.2V12h2.2l-.4 3h-1.8v7A10 
+          10 0 0022 12z"/>
+        </svg>
+      ),
+    },
+    {
+      name: 'TikTok',
+      url: 'https://www.tiktok.com/@tiramisu.bar.split',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
+          <path d="M21 8.5a5.5 5.5 0 01-4.3-2.2v7.1a5.6 5.6 0 11-5.6-5.6c.3 0 
+          .6 0 .9.1v2.3a3.3 3.3 0 102.3 3.1V2h2.3c.2 1.2.8 2.3 
+          1.7 3.1A5.5 5.5 0 0121 6.2v2.3z"/>
+        </svg>
+      ),
+    },
+  ],
+  copyright: 'Tiramisu. Sva prava pridržana.',
+};
